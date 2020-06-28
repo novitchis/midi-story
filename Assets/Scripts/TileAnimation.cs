88a11c3a79d4d5e9cb80a86ba3a264cfbbcf7040
Silviu <silviu.novitchi@cti.pub.ro>
2020-06-28 20:10:22 +0300

    Improve pressed tile graphics.

---
+++ Assets/Scripts/TileAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileAnimation : MonoBehaviour
+{
+    private Material material = null;
+    private float distance = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = this.GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y < 0) {
+            distance = Mathf.Min(distance + Time.deltaTime * 2, 0.2f);
+
+            material.SetFloat("Vector1_8C03E553", distance);
+        }
+    }
+}
